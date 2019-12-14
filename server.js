bab5b2d0d096c21a735bb43183bfa58f7585ea55
Camilo Caroso <camilitosamuel@hotmail.com>
2019-12-14 11:57:13 -0500

    server

--- server.js
+++ server.js
@@ -5,9 +5,26 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
+const reservedTable = [{
+    name: "yoda",
+    email: "yoda@sw.com",
+    id: 1,
+    phone: 3051234567
+}];
+
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "public/main.html"));
 });
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/reserve.html"));
+});
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/tables.html"));
+});
+
+app.get("/api/tables", function(req, res) {
+    res.json(reservedTable);
+  });
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT ${PORT}`);
