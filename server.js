bdd377611178e5b51c6a054184a1ea3eeafcf68b
Camilo Caroso <camilitosamuel@hotmail.com>
2019-12-14 12:15:40 -0500

    server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const reservedTable = [{
 }];
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
