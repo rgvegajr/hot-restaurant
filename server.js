7fef49467bd19a6ddb3b7f06f41f4e19fe45cfeb
RV <rgvegajr@gmail.com>
2019-12-14 12:43:20 -0500

    revised server.js, added mysql to repo

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const mysql = require("mysql");
+
+//user should edit and save the following database connection code with appropriate host,port,user,password, and database specifics
+const connection = mysql.createConnection({ //creatConnection function takes in object with host, port, un, pw, db name, etc
+    host: "localhost", //url where databse lives
+    // your port; if not 3306
+    port: 3306,
+    // your username
+    user: "root",
+    //yourpassword
+    password: "password1234",
+    database: "reservations_db"
+});
+
+function afterConnection() {
+    connection.query("SELECT * FROM tables", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+    });
+}
+
+//establish connection and call functions
+connection.connect(function(err) { //before calling any other functions to query database, etc, must ensure a connection
+    if (err) throw err;
+    afterConnection();
+});
 
 const app = express();
 const PORT = 3000;
@@ -24,7 +50,7 @@ app.get("/tables", function(req, res) {
 
 app.get("/api/tables", function(req, res) {
     res.json(reservedTable);
-  });
+});
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT ${PORT}`);
